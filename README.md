ef34accff8bed39ed500c1fd3c04956260287f56
pianomania <cpy584136@gmail.com>
2018-10-04 19:34:27 +0800

    update readme and  nac.py

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # NALU-tensorflow
-A Tensorflow Implementation of "Neural Arithmetic Logic Units"
+A Tensorflow Implementation of [Neural Arithmetic Logic Units](https://arxiv.org/abs/1808.00508)
+
+# Environment
+Ubuntu 18<br/>
+Tensorflow 1.10.1<br/>
+numpy 1.14.5<br/>
+
+# Result
+- **Synthetic Arithmetic Tasks**
+
+|op|Static Task|
+|-----|-----|
+
+- **MNIST Counting**
+ 
--- nac.py
+++ nac.py
@@ -1,25 +1,24 @@
 import tensorflow as tf
-from tensorflow import keras
-import numpy as np
-import tensorflow.contrib.eager as tfe
 
-tf.enable_eager_execution()
 
-var_init = lambda x: tf.variance_scaling_initializer(scale=x, distribution='uniform')
+var_init = lambda x: tf.variance_scaling_initializer(scale=x, distribution='truncated_normal')
+scale = 1
 
 class NACCell(object):
 
     def __init__(self, in_dim, out_dim):
 
-        with tf.variable_scope("nac-cell"):
-            self.w = tf.get_variable(name = 'w', shape = [in_dim, out_dim], initializer=var_init(1.0))
-            self.m = tf.get_variable(name = 'm', shape = [in_dim, out_dim], initializer=var_init(1.0))
-            self.b = tf.get_variable(name = 'b', shape = [1, out_dim])
+        
+        self.w = tf.get_variable(name = 'w', shape = [in_dim, out_dim], initializer=var_init(scale))
+        self.m = tf.get_variable(name = 'm', shape = [in_dim, out_dim], initializer=var_init(scale))
+        self.b = tf.get_variable(name = 'b', shape = [1, out_dim])
+
+        self.var_list = [self.w, self.m, self.b]
 
     def __call__(self, x):
 
         w = tf.multiply(tf.tanh(self.w), tf.sigmoid(self.m))
-        y = tf.matmul(x, w) + self.b
+        y = tf.matmul(x, w) #+ self.b
 
         return y
 
@@ -29,16 +28,18 @@ class NAC(object):
     def __init__(self, in_dim=2, out_dim=1, hidden_dim=2, n_stacks=2):
 
         self.layers = []
+        self.var_list = []
 
-        with tf.variable_scope("nac"):
-
-            for i in range(n_stacks):
-                    self.layers.append(
-                        NACCell(
-                            in_dim if i == 0 else hidden_dim, 
-                            out_dim if i == n_stacks-1 else hidden_dim
-                        )
+        for i in range(n_stacks):
+                self.layers.append(
+                    NACCell(
+                        in_dim if i == 0 else hidden_dim, 
+                        out_dim if i == n_stacks-1 else hidden_dim
                     )
+                )
+
+        for layer in self.layers:
+            self.var_list += layer.var_list
 
     def __call__(self, x):
 
@@ -49,20 +50,24 @@ class NAC(object):
 
         return y
 
+
 class NALUCell(object):
 
-    def __init__(self, in_dim, out_dim, eps=10**-5):
+    def __init__(self, in_dim, out_dim, eps=1e-5):
         
-        with tf.variable_scope("nalu-cell"):
-            self.add_cell = NACCell(in_dim, out_dim)
-            self.mul_cell = NACCell(in_dim, out_dim)
-            self.G = tf.get_variable('g', shape = [in_dim, out_dim], initializer=var_init(1/3))
-            self.eps = eps
+        
+        self.add_cell = NACCell(in_dim, out_dim)
+        self.mul_cell = NACCell(in_dim, out_dim)
+        self.G = tf.get_variable('g', shape = [in_dim, out_dim], initializer=var_init(scale))
+        self.eps = eps
+        
+        self.var_list = self.add_cell.var_list + self.mul_cell.var_list + [self.G]
 
     def __call__(self, x):
 
         a = self.add_cell(x)
         m = self.mul_cell(tf.log(tf.abs(x) + self.eps))
+        m = tf.exp(m)
         g = tf.sigmoid(tf.matmul(x, self.G))
         y = tf.multiply(g, a) + tf.multiply(1-g, m)
 
@@ -73,15 +78,18 @@ class NALU(object):
     def __init__(self, in_dim, out_dim, hidden_dim, n_stacks, eps):
 
         self.layers = []
+        self.var_list = []
 
-        with tf.variable_scope("nalu"):
-            for i in range(n_stacks):
-                self.layers.append(
-                    NALUCell(
-                        in_dim if i == 0 else hidden_dim,
-                        out_dim if i == n_stacks-1 else hidden_dim
-                    )
+        for i in range(n_stacks):
+            self.layers.append(
+                NALUCell(
+                    in_dim if i == 0 else hidden_dim,
+                    out_dim if i == n_stacks-1 else hidden_dim
                 )
+            )
+
+        for layer in self.layers:
+            self.var_list += layer.var_list
 
     def __call__(self, x):
 
@@ -91,66 +99,3 @@ class NALU(object):
             y = layer(y)
 
         return y
-
-
-def loss_fn(nac, x, target_y):
-    y = nac(x)
-    return tf.reduce_mean(tf.pow(target_y - y, 2)) 
-
-# add_op = lambda x, y: x + y
-mul_op = lambda x, y: x*y
-add_op = mul_op
-
-
-if __name__ == "__main__":
-
-     with tf.device("/gpu:0"):
-        optim = tf.train.AdamOptimizer(0.01)
-
-        x1 = tf.random_uniform([100, 1], 1, 10)
-        x2 = tf.random_uniform([100, 1], 1, 10)
-        target_y = add_op(x1, x2)
-        x = tf.concat([x1, x2], axis=1)
-
-        # nac = NAC()
-        # nac = NACCell(2, 1)
-        # nac = NALUCell(2, 1)
-        nac = NALU(2, 1, 2, 2, 10**-5)
-
-        with tf.device("/gpu:0"):
-            for i in range(50000):
-
-                loss = loss_fn(nac, x, target_y)
-                optim.minimize(lambda :loss_fn(nac, x, target_y))
-
-                print("%.7f" % loss.numpy())
-
-        # test case
-        x = tf.constant([[1, 1], [2, 1], [3, 2], [11, 10]], dtype=tf.float32)
-        y = nac(x)
-        print(y.numpy())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
